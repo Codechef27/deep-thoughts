fef1e4d88c7dda1a72a493d0ea1f61703bed4850
BradleyK <bradleyjkennett@gmail.com>
2022-08-31 11:26:07 -0400

    node modules add to gitignore

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
-const { User, Thought } = require('../models')
+const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -25,6 +26,31 @@ const resolvers = {
           .populate('thoughts');
       },
 
+    },
+
+    Mutation: {
+        addUser: async ( parent, args ) => {
+            const user = await User.create(args);
+
+            return user;
+        },
+
+        login: async (parent, { email, password }) => {
+
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                throw new AuthenticationError('incorrect email or password')
+            }
+
+            const correctPW = await user.isCorrectPassword(password);
+
+            if(!correctPW) {
+                throw new AuthenticationError('incorrect email or password')
+            }
+
+            return user;
+        }
     }
   };
   
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,7 +31,12 @@ type Query {
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
-    thought(_id: ID): Thought
+    thought(_id: ID!): Thought
+}
+
+type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
 }
 
 `;
